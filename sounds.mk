71e076d9edd6255790261bcf9710d6128398a393
Dil3mm4 <dil3mm4.dev@gmail.com>
2019-02-03 22:21:14 +0000

    Initial commit - Including Google's UI sounds

---
+++ sounds.mk
@@ -0,0 +1,39 @@
+# Copyright 2019 Descendant 
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_COPY_FILES += \
+    vendor/sounds/ui/audio_initiate.ogg:system/media/audio/ui/audio_initiate.ogg \
+    vendor/sounds/ui/camera_click.ogg:system/media/audio/ui/camera_click.ogg \
+    vendor/sounds/ui/camera_focus.ogg:system/media/audio/ui/camera_focus.ogg \
+    vendor/sounds/ui/ChargingStarted.ogg:system/media/audio/ui/ChargingStarted.ogg \
+    vendor/sounds/ui/Dock.ogg:system/media/audio/ui/Dock.ogg \
+    vendor/sounds/ui/Effect_Tick.ogg:system/media/audio/ui/Effect_Tick.ogg \
+    vendor/sounds/ui/KeypressDelete.ogg:system/media/audio/ui/KeypressDelete.ogg \
+    vendor/sounds/ui/KeypressInvalid.ogg:system/media/audio/ui/KeypressInvalid.ogg \
+    vendor/sounds/ui/KeypressReturn.ogg:system/media/audio/ui/KeypressReturn.ogg \
+    vendor/sounds/ui/KeypressSpacebar.ogg:system/media/audio/ui/KeypressSpacebar.ogg \
+    vendor/sounds/ui/KeypressStandard.ogg:system/media/audio/ui/KeypressStandard.ogg \
+    vendor/sounds/ui/Lock.ogg:system/media/audio/ui/Lock.ogg \
+    vendor/sounds/ui/LowBattery.ogg:system/media/audio/ui/LowBattery.ogg \
+    vendor/sounds/ui/NFCFailure.ogg:system/media/audio/ui/NFCFailure.ogg \
+    vendor/sounds/ui/NFCInitiated.ogg:system/media/audio/ui/NFCInitiated.ogg \
+    vendor/sounds/ui/NFCSuccess.ogg:system/media/audio/ui/NFCSuccess.ogg \
+    vendor/sounds/ui/NFCTransferComplete.ogg:system/media/audio/ui/NFCTransferComplete.ogg \
+    vendor/sounds/ui/NFCTransferInitiated.ogg:system/media/audio/ui/NFCTransferInitiated.ogg \
+    vendor/sounds/ui/Trusted.ogg:system/media/audio/ui/Trusted.ogg \
+    vendor/sounds/ui/Undock.ogg:system/media/audio/ui/Undock.ogg \
+    vendor/sounds/ui/Unlock.ogg:system/media/audio/ui/Unlock.ogg \
+    vendor/sounds/ui/VideoRecord.ogg:system/media/audio/ui/VideoRecord.ogg \
+    vendor/sounds/ui/VideoStop.ogg:system/media/audio/ui/VideoStop.ogg \
+    vendor/sounds/ui/WirelessChargingStarted.ogg:system/media/audio/ui/WirelessChargingStarted.ogg \
